ed1a87784b67da892ec28ed50bda5d12a8190f47
ASHISH YADAV <nowwhoisashish@gmail.com>
2020-08-13 08:34:04 +0530

    Add files via upload

---
+++ addons/classes/Constants.php
@@ -0,0 +1,18 @@
+<?php
+
+class Constants{
+	public static $passwordsDoNotMatch = "Your Passwords do not Match!";
+	public static $passwordNotAlphanumeric = "Your Password must only consist of letters and numbers!";
+	public static $passwordCharacters = "Your Password must be between 5 to 30 characters!";
+	public static $emailInvalid = "Your Email is invalid!";
+	public static $emailTaken = "That Email is already registered with us! Try Logging In";
+	public static $emailsDoNotMatch = "Your Emails do not Match!";
+	public static $lastNameCharacters = "Your lastname must be between 2 to 25 characters long";
+	public static $firstNameCharacters = "Your firstname must be between 2 to 25 characters long";
+	public static $usernameCharacters = "Your username must be between 5 to 25 characters long";
+    public static $usernameTaken = "That username is already assigned to someone else, try something cooler!";
+    
+    public static $loginFailed = "Your Username or Password was incorrect!";
+}
+
+?>
